cf0f9fd8c23371e0f3db46f14b30e9bc617c4853
Noah Yost <bl4cksn0wgames@gmail.com>
2016-04-20 10:26:20 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*********************************************************************************
+* Hadrian's Wall
+* Development Start: 4/20/2016
+* Development Finish:
+* ALL RIGHTS RESERVED (C) 2016 BLACKSNOW GAMES
+* ********************************************************************************
+* File:         main.cpp
+* Author:       Noah Yost
+* Date:         4/20/2016
+* Description:  
+**********************************************************************************/
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hadrian's Wall" << std::endl;
+  return 0;
+}
